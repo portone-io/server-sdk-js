7df4a38c02d7120c6cbc7b89e5c08f5a40dadd1a
XiNiHa <me@xiniha.dev>
2024-05-30 20:16:21 +0900

    예제 업데이트

--- examples/express/index.js
+++ examples/express/index.js
@@ -3,9 +3,29 @@ const PortOne = require("@portone/server-sdk");
 
 const app = express();
 
+const webhookSecret = "pzQGE83cSIRKM4/WH5QY+g==";
+
 app.get("/", (_, res) => {
 	if (PortOne) res.end("PortOne Server SDK is available");
 	else res.end("PortOne Server SDK is not available");
+});
+
+app.get("/webhook", express.text(), (req, res) => {
+	PortOne.Webhook.verify(webhookSecret, req.body, req.headers).then(
+		(payload) => {
+			console.log("Valid webhook payload received:", payload);
+			res.status(200).end();
+		},
+		(error) => {
+			if (error instanceof PortOne.Webhook.WebhookVerificationError) {
+				console.log("Invalid webhook payload received:", error.message);
+				res.status(400).end();
+			} else {
+				console.error(error);
+				res.status(500).end();
+			}
+		},
+	);
 });
 
 app.listen(8080, () => {
