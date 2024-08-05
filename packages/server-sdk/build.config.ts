857292fa42a6ef667783998a9bdd4de21e22375a
kiwiyou <kiwiyou@portone.io>
2024-08-05 17:22:16 +0900

    feat: set user-agent of client

--- packages/server-sdk/build.config.ts
+++ packages/server-sdk/build.config.ts
@@ -1,5 +1,12 @@
+import childProcess from "node:child_process";
+import fs from "node:fs/promises";
 import { type MkdistBuildEntry, defineBuildConfig } from "unbuild";
 import rootTsConfig from "../../tsconfig.json";
+import packageInfo from "./package.json";
+const commitHash = childProcess
+	.execSync("git rev-parse HEAD", { encoding: "utf-8" })
+	.trim();
+const userAgent = `"portone-io/server-sdk-js v${packageInfo.version} (${commitHash})"`;
 
 export default defineBuildConfig({
 	entries: [
@@ -22,4 +29,17 @@ export default defineBuildConfig({
 			}),
 		),
 	],
+	hooks: {
+		"mkdist:entry:build": async (_ctx, _entry, output) => {
+			await Promise.all(
+				output.writtenFiles.map(async (file) => {
+					const content = await fs.readFile(file, { encoding: "utf-8" });
+					return fs.writeFile(
+						file,
+						content.replaceAll("__USER_AGENT__", userAgent),
+					);
+				}),
+			);
+		},
+	},
 });
---
+++ packages/server-sdk/src/global.d.ts
@@ -0,0 +1,1 @@
+declare const __USER_AGENT__: string;
