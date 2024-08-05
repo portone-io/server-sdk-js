b4b2c6bad121066adfc29a9fb25868335a11b30b
CirnoV (Sickle) <cirnov59@gmail.com>
2024-08-05 12:26:11 +0900

    패키지 업데이트 및 개발 환경 개선 (#4)
    * pnpm 9.6.0
    
    * 패키지 업데이트
    
    * biome vscode 설정 보완 및 vcs integration 적용
    
    * unbuild 빌드 오류 해결
    
    * 테스트 오류 수정
    
    * express 예제 패키지 업데이트
    
    * update pnpm-lock.yaml
    
    * `server.fs.cachedChecks` 주석 추가
    
    * lint 수정

--- packages/server-sdk/vitest.config.ts
+++ packages/server-sdk/vitest.config.ts
@@ -1,3 +1,10 @@
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({});
+export default defineConfig({
+	server: {
+		fs: {
+			// https://github.com/vitest-dev/vitest/issues/6152
+			cachedChecks: false,
+		},
+	},
+});
