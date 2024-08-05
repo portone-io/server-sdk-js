eb88f9a81956f4fc9ccb18dc387641c42989717e
CirnoV <cirnov59@gmail.com>
2024-08-05 12:14:02 +0900

    `server.fs.cachedChecks` 주석 추가

--- packages/server-sdk/vitest.config.ts
+++ packages/server-sdk/vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	server: {
 		fs: {
+			// https://github.com/vitest-dev/vitest/issues/6152
 			cachedChecks: false,
 		},
 	},
