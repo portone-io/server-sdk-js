6b8bd525a9ce8a86d36744a8a07bb6618a95f753
kiwiyou <kiwiyou@portone.io>
2024-08-06 11:48:55 +0900

    feat: setup typedoc

---
+++ packages/server-sdk/typedoc.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('typedoc').TypeDocOptions} */
+module.exports = {
+	entryPoints: ["./src/index.ts"],
+	emit: "docs",
+	out: "./docs",
+	tsconfig: "../../tsconfig.json",
+	plugin: ["./typedoc-plugin-expand.mjs"],
+};
